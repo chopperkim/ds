092d9fd3caedf3a278d8cb344166cc59167c4dca
chopperkim <chocojhkim@gmail.com>
2023-07-05 20:46:59 +0900

    cnn + 자동차 번호 인식

---
+++ chapter08/mnist-keras.py
@@ -0,0 +1,48 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.datasets import mnist
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import numpy as np
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1).astype('float32') / 255
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1).astype('float32') / 255
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(28, 28, 1), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model_path = "mnist_cnn.hdf5"
+check_pointer = ModelCheckpoint(filepath=model_path, monitor='val_loss', verbose=1, save_best_only=True)
+early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10)
+
+history = model.fit(X_train, y_train, validation_split=0.25, epochs=30, batch_size=200, verbose=0, callbacks=[early_stopping_callback, check_pointer])
+
+print("\n Test Accuracy: %.4f" % (model.evaluate(X_test, y_test)[1]))
+
+y_vloss = history.history['val_loss']
+y_loss = history.history['loss']
+
+x_len = np.arange(len(y_loss))
+plt.plot(x_len, y_vloss, marker='.', c='red', label='Testset_loss')
+plt.plot(x_len, y_loss, marker='.', c='blue', label='Trainset_loss')
+
+plt.legend(loc='upper right')
+plt.grid()
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
+
